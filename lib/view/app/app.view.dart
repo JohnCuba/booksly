126d2a7691eb309fd05b1156d43214e149d5be79
JohnCuba <ekubetsky@gmail.com>
2023-02-28 22:33:09 +0300

    refactor: rename file

--- lib/view/app/app.view.dart
+++ lib/view/app/app.view.dart
@@ -1,7 +1,6 @@
 import 'package:booksly/config/injector.dart';
-import 'package:booksly/config/localization.config.dart';
+import 'package:booksly/config/localization.dart';
 import 'package:booksly/view/app/navigation.cubit.dart';
-import 'package:booksly/view/pages/local_library/local_library.page.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
